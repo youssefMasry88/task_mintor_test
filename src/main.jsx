c6c10f596812af237d4759a49bb51a695339c321
Youssef Masry <youssefmasry888@gmail.com>
2026-02-03 01:19:15 +0200

    test version change

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+if (!window.location.hash) {
+  window.location.replace(window.location.href + "#/");
+}
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
